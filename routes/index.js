780371a18be83c3637050422f1b94972fd88d3e0
mayurshedage <mayur.shedage@inscripts.in>
2021-10-16 19:15:28 +0530

    url error handling

--- routes/index.js
+++ routes/index.js
@@ -28,10 +28,14 @@ module.exports = (app, req, res) => {
             req['debug'] = 1;
         }
 
-        if (routes.hasOwnProperty(subdomainPrefix)) {
+        if (routes.hasOwnProperty(subdomainPrefix) && routePath != '/') {
             const currentRoute = routePath.split('/')[2].slice(0, -1);
 
-            require('./' + routePath.split('/')[1] + '/' + routes[subdomainPrefix] + '/' + routes[subdomainPrefix])(app, req, res, currentRoute);
+            try {
+                require('./' + routePath.split('/')[1] + '/' + routes[subdomainPrefix] + '/' + routes[subdomainPrefix])(app, req, res, currentRoute);
+            } catch (error) {
+                throw new Error('Request URL not found');
+            }
         }
     } else {
         throw new Error('Request URL not found');
