a0e657aaf21003758b6791e3922dcb566d2d4ff6
mayurshedage <mayur.shedage@inscripts.in>
2021-10-21 19:59:48 +0530

    updated test

--- tests/apikey.test.js
+++ tests/apikey.test.js
@@ -13,7 +13,7 @@ describe('APIKeyModel', () => {
     describe('/GET apikeys', () => {
         it('it should GET all the apikeys', (done) => {
             chai.request('http://2138954385ae02.chat-us.cometondemand.com:8000/')
-                .get('v1.0/apikeys')
+                .get('v1.0/admin/apikeys')
                 .set('apiKey', 'd027ffb7a626ed5ae7ddf186efa1ed3ccba3ada6')
                 .end((err, res) => {
                     console.log(res.body);
